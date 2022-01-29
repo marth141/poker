231256a0f2ebab69b6079b2daa9e4b28f25184bb
marth141 <ncasados@live.com>
2022-01-28 19:19:26 -0700

    Making withs

--- lib/poker.ex
+++ lib/poker.ex
@@ -24,29 +24,60 @@ defmodule Poker do
     end
   end
 
+  def view_deck(deck_server) do
+    with {:ok, deck} <- GenServer.call(deck_server, :view) do
+      {:ok, deck}
+    else
+      e -> e
+    end
+  end
+
+  def count_deck(deck_server) do
+    with {:ok, count} <- GenServer.call(deck_server, :count) do
+      {:ok, count}
+    else
+      e -> e
+    end
+  end
+
   def give_me_a_hand(deck_server) do
-    {:ok, c1} = give_me_a_card(deck_server)
-    {:ok, c2} = give_me_a_card(deck_server)
-    {:ok, c1 ++ c2}
+    with {:ok, c1} <- give_me_a_card(deck_server),
+         {:ok, c2} <- give_me_a_card(deck_server) do
+      {:ok, c1 ++ c2}
+    else
+      e -> e
+    end
   end
 
   def give_me_a_card(deck_server) do
-    {:ok, c} = GenServer.call(deck_server, :draw)
-    {:ok, c}
+    with {:ok, c} <- GenServer.call(deck_server, :draw) do
+      {:ok, c}
+    else
+      e -> e
+    end
   end
 
   def make_a_deck_server do
-    {:ok, deck_server} = GenServer.start_link(Poker.DeckServer, :ok)
-    {:ok, deck_server}
+    with {:ok, deck_server} <- GenServer.start_link(Poker.DeckServer, :ok) do
+      {:ok, deck_server}
+    else
+      e -> e
+    end
   end
 
   def make_a_deck do
-    {:ok, deck} = Poker.Deck.create()
-    {:ok, deck}
+    with {:ok, deck} <- Poker.Deck.create() do
+      {:ok, deck}
+    else
+      e -> e
+    end
   end
 
   def make_a_shuffled_deck() do
-    {:ok, deck} = make_a_deck() |> Poker.Deck.shuffle()
-    {:ok, deck}
+    with {:ok, deck} <- make_a_deck() |> Poker.Deck.shuffle() do
+      {:ok, deck}
+    else
+      e -> e
+    end
   end
 end
